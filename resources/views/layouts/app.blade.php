47d696c259c963b9117913dfc030eee7c3b113c3
ricardosawir <sawir.ricardo@gmail.com>
2021-05-19 16:13:03 +0700

    added google analytic to measure page view

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,6 +12,15 @@
             href="//fonts.googleapis.com/css?family=Titillium+Web:700|Source+Serif+Pro:400,700|Merriweather+Sans:400,700|Source+Sans+Pro:400,300,600,700,300italic,400italic,600italic,700italic"
             rel="stylesheet" type="text/css">
         <link rel="stylesheet" href="//demo.productionready.io/main.css">
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-HK4XWWXRK0"></script>
+        <script>
+            window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-HK4XWWXRK0');
+        </script>
     </head>
 
     <body>
