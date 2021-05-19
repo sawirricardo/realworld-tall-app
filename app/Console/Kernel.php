da9142e0c05cf472deb87c86e23b6a7466eb57dc
ricardosawir <sawir.ricardo@gmail.com>
2021-05-19 11:45:22 +0700

    changed readme

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,6 +25,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
+        $schedule->command('migrate:fresh --seed')->daily();
     }
 
     /**
@@ -34,7 +35,7 @@ class Kernel extends ConsoleKernel
      */
     protected function commands()
     {
-        $this->load(__DIR__.'/Commands');
+        $this->load(__DIR__ . '/Commands');
 
         require base_path('routes/console.php');
     }
